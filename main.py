c61e25cccfc1074a83fcfe969b068380c0009d67
Roman Medvedev <roma.medvedev@mail.ru>
2024-11-03 12:41:22 +0300

    Fixes main.py, tests.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from src.masks import get_masks_account, get_masks_card_number
+from src.widget import get_date, masks_account_card
 
 print(get_masks_card_number(7000792289606361))
 
 print(get_masks_account(73654108430135874305))
+
+print(get_date("2024-03-11T02:26:18.671407"))
+
+print(masks_account_card('Maestro 1596837868705199'))
+
--- tests/tests.py
+++ tests/tests.py
@@ -1,4 +1,5 @@
 from src.masks import get_masks_account, get_masks_card_number
+from src.widget import get_date, masks_account_card
 
 
 def test_function(number, function, data):
@@ -32,6 +33,7 @@ test_function(4, get_masks_card_number, "A000792289606361")
 
 # Задаем функции get_masks_card_number номер карты с неверным типом данных (тип list)
 test_function(5, get_masks_card_number, [1, 2, 3])
+print()
 
 print("Функция get_masks_account:")
 # Задаем функции get_masks_card_account номер счета в верном формате (тип int)
@@ -45,3 +47,30 @@ test_function(3, get_masks_account, "A3654108430135874305")
 
 # Задаем функции get_masks_card_account номер счета с неверным типом данных (тип list)
 test_function(4, get_masks_account, [1, 2, 3])
+print()
+
+print('Функция get_data:')
+test_function(1, get_date, "2024-03-11T02:26:18.671407")
+print()
+
+print("Функция get_account_card:")
+test_function(1, masks_account_card, 'Maestro 1596837868705199')
+test_function(2, masks_account_card, 'Счет 64686473678894779589')
+test_function(3, masks_account_card, 'MasterCard 7158300734726758')
+test_function(4, masks_account_card, 'Счет 35383033474447895560')
+test_function(5, masks_account_card, 'Visa Classic 6831982476737658')
+test_function(6, masks_account_card, 'Visa Platinum 8990922113665229')
+test_function(7, masks_account_card, 'Visa Gold 5999414228426353')
+test_function(8, masks_account_card, 'Счет 73654108430135874305')
+
+#Задаем функции masks_account_card номер карты в неверном формате (менее 16 символов)
+test_function(9, masks_account_card, 'Maestro 868705199')
+
+#Задаем функции masks_account_card номер карты в неверном формате (буква в номере)
+test_function(10, masks_account_card, 'Maestro A596837868705199')
+
+#Задаем функции masks_account_card номер карты в неверном формате (тип данных не str)
+test_function(11, masks_account_card, [1, 2, 3])
+
+#Задаем функции masks_account_card номер счета в неверном формате (буква в номере)
+test_function(12, masks_account_card, 'Счет 73A54108430135874305')
